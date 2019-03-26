265b6910b3b2bd8abd7657ecf9df81409722b288
Jay Neupane <jayesh.neupane@gmail.com>
2019-03-25 19:46:47 -0500

    commit

---
+++ models/airport.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+  var Airport = sequelize.define(
+    "Airport",
+    {
+      id: DataTypes.INTEGER(15),
+      CityId: DataTypes.STRING(45),
+      CityName: DataTypes.STRING(45),
+      CountryName: DataTypes.STRING(100),
+      IataCode: DataTypes.STRING(6),
+      Name: DataTypes.STRING(20),
+      PlaceId: DataTypes.INTEGER(20),
+      SkyscannerCode: DataTypes.STRING(20),
+      Type: DataTypes.STRING(20)
+    },
+
+    {
+      timestamps: false,
+      tableName: "airport"
+    }
+  );
+
+  return Airport;
+};
