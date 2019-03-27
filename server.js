c9e47a2c2a95fb551a4406249934010fab86f699
hyryo513 <henry.heyi513@gmail.com>
2019-03-27 18:17:25 -0500

    removed unnecessary package and console logs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var cors = require('cors');
 
 //var db = require("./models");
 
@@ -11,7 +10,6 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors({origin: 'http://localhost:3000'}));
 
 // Routes
 require("./routes/apiRoutes")(app);
