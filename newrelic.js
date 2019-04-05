6ffaa10c418434aa62b02e7793ca352d6ba937fc
hyryo513 <henry.heyi513@gmail.com>
2019-04-05 01:02:46 -0500

    updated the name of key

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ require("dotenv").config();
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.newrelicKey
+  license_key: process.env.NEW_RELIC_LICENSE_KEY
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
