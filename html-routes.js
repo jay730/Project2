fdd13921e2b25927b4a92cd3dcf3cde21d70abd0
Johnny Laberda <45076635+JohnnyLaberda@users.noreply.github.com>
2019-04-06 10:33:06 -0500

    Add files via upload

--- html-routes.js
+++ html-routes.js
@@ -3,24 +3,23 @@
 var path = require("path");
 
 module.exports = function(app) {
+
   // Load index page
   app.get("/", function(req, res) {
+      
     var indexHtml = path.join(__dirname, '../public/html/index.html');
     res.sendFile(indexHtml);
   });
 
   //Keep for reference
   // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
+     app.get("/example/:id", function(req, res) {
+       db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+         res.render("example", {
+           example: dbExample
+         });
+       });
+    });
 
-  // // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
+
 };
